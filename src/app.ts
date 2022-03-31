707823dd21e5f3c7f480340d7043ef83337f8718
LopitB <lopitbohdan@gmail.com>
2022-03-31 14:28:51 +0300

    Lesson Five

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'reflect-metadata';
+import {createConnection} from 'typeorm';
+
+const apiRoutes = require('./routes/apiRoutes');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(apiRoutes);
+
+app.listen(5500, async () => {
+  console.log('Server started!');
+
+  try {
+    const connection = await createConnection();
+    if (connection) {
+      console.log('Database connected');
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
